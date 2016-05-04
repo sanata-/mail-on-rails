b20ba5821d05ec5bf1870b8d37a15065d3102f60
osfx <x66w@yandex.ru>
2016-05-04 10:44:22 +0300

    All mailing list methods

--- app/services/mailing_list_service.rb
+++ app/services/mailing_list_service.rb
@@ -17,4 +17,35 @@ class MailingListService < ApiService
     result = HTTParty.post(request_url, :query => {:domain => @domain, :maillist => maillist }, :headers => { 'PddToken' => @token })
     @response = JSON.parse(result.body)
   end
+
+  def subscribe(maillist, subscriber, status)
+    request_url = @url + "/api2/admin/email/ml/subscribe"
+    result = HTTParty.post(request_url, :query => {:domain => @domain , maillist: maillist, subscriber: subscriber, can_send_on_behalf: status }, :headers => { 'PddToken' => @token })
+    @response = JSON.parse(result.body)
+  end
+
+  def subscribers(maillist)
+    request_url = @url + "/api2/admin/email/ml/subscribers?"
+    result = HTTParty.post(request_url, :query => {:domain => @domain , maillist: maillist}, :headers => { 'PddToken' => @token })
+    @response = JSON.parse(result.body)
+  end
+
+  def unsubscribe(maillist, subscriber)
+    request_url = @url + "/api2/admin/email/ml/unsubscribe"
+    result = HTTParty.post(request_url, :query => {:domain => @domain , maillist: maillist, subscriber: subscriber }, :headers => { 'PddToken' => @token })
+    @response = JSON.parse(result.body)
+  end
+
+  def status_get(maillist,subscriber)
+    request_url = @url + "/api2/admin/email/ml/get_can_send_on_behalf"
+    result = HTTParty.post(request_url, query: {domain: @domain, maillist: maillist, subscriber: subscriber}, headers: { 'PddToken' => @token })
+    @response = JSON.parse(result.body)
+  end
+
+  def status_set(maillist, subscriber, status)
+    request_url = @url + "/api2/admin/email/ml/set_can_send_on_behalf"
+    result = HTTParty.post(request_url, query: {domain: @domain, maillist: maillist, subscriber: subscriber, can_send_on_behalf: status}, headers: { 'PddToken' => @token })
+    @response = JSON.parse(result.body)
+  end
+
 end
